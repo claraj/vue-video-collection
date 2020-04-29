18023203b2e4c068445969a4fa9a3027b5c2568d
clara <clara@minneapolis.edu>
2020-04-28 21:09:49 -0500

    improve URL validation somewhat

--- src/components/AddActivity.vue
+++ src/components/AddActivity.vue
@@ -34,27 +34,49 @@ export default {
   },
   methods: {
     add() {
+
       this.errors = []
+
+      let videoId 
 
       if (!this.title) {
         this.errors.push('Enter a title')
       }
 
-      // Example video URL is https://www.youtube.com/watch?v=aAxGTnVNJiE
-      // Notice the ID at the end of the URL. This uniquely identifes a video
-      // at YouTube, so this is what we'll store 
-
-      // TODO check that the URL provided is a valid one 
-      let videoId = this.url.split('?v=')[1]   
+      /* Example video URL is https://www.youtube.com/watch?v=aAxGTnVNJiE
+      
+      Notice the ID at the end of the URL. This uniquely identifes a video
+      at YouTube, so this is what we'll store in an activity object
+      In a URL, everything after a ? is a set of key=value pairs, often refered
+      to as params, or parameters, or search parameters. 
+      The search params in the examples URL above are 
+      
+      v=aAxGTnVNJiE
+      
+      We can use the built-in URL and URLSearchParams to extract the ID
+      from the URL. 
+      
+      */
 
       if (!this.url) {
         this.errors.push('Enter a video URL')
       }
-      
-      if (this.url && !videoId) {
-        this.errors.push("Please check video URL")
+
+      else {  // there is a url, is it valid?
+        try {
+          let url = new URL(this.url)   
+          let params = new URLSearchParams(url.search)
+          videoId = params.get('v')  // key is the v in (for example) v=aAxGTnVNJiE
+          if (!videoId) {
+            this.errors.push('Enter a valid YouTube URL')
+          }  
+        }
+        catch (TypeError) {  // thrown if the URL is not a valid format.  
+          this.errors.push('Enter a valid YouTube URL')
+        }
       }
 
+      // Finally, if there are no errors detected... 
       if (this.errors.length === 0) {
         let activity = { title: this.title, youtubeId: videoId }
           this.$emit('new-activity', activity)
